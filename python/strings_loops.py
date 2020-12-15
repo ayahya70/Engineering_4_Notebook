40602dd3668c4dc9758e43f23a1d89340b91648e
ayahya70 <54993981+ayahya70@users.noreply.github.com>
2020-12-15 09:36:22 -0500

    Update strings_loops.py

--- python/strings_loops.py
+++ python/strings_loops.py
@@ -1,9 +1,9 @@
-sentence = input("Type a simple sentence: ")
-split_sentence = sentence.split(" ")
+sentence = input("Type a simple sentence: ")  # this is where you insret your senetence
+split_sentence = sentence.split(" ")  # split sentences
 
-for word in split_sentence:
+for word in split_sentence:   # loops over words
 
-    for character in word:
+    for character in word:  # loops over character
         print(character)
     print("-")
 
